558e0d1bfcf2103fef5dfc9425bbc4de358f14ff
Muhammad Elsadany <muhammad-elsadany@uiowa.edu>
2024-03-05 13:16:08 -0600

    edited some language metrics corr with IQ

--- language/src/05_PS-VC-mtrics-to-IQ.R
+++ language/src/05_PS-VC-mtrics-to-IQ.R
@@ -193,17 +193,38 @@ ggsave("figs/corr-iq-nih-averaged-metrics.png", bg = "white",
 # try to check how these metrics can predict IQ/NIH-TB, but do a lmer instead of spearman
 m124 <- full_join(all, combined.metrics) %>%
   distinct() %>%
-  full_join(m1.m2) %>% ungroup() %>% distinct() 
+  full_join(m1.m2) %>% ungroup() %>% distinct()  %>%
+  left_join(demo[,c(2,4,5)])
 write_rds(m124, "data/derivatives/all-summarized-metrics.rds")
-lmer.results <- foreach(i = 18:ncol(m124), .combine = rbind) %dopar% {
+# make heatmap of correlation between vars
+corr.table(m124 %>% 
+             select(3:10),
+           m124 %>%
+             select(11:16)) %>%
+  mutate(FDR = p.adjust(pval, method = "fdr")) %>%
+  filter(V1 %in% colnames(m124)[3:16],
+         V2 %in% colnames(m124)[3:16],
+         pval > 0) %>%
+  ggplot(aes(x=V2, y=V1, fill = r, label = ifelse(FDR<0.05, "**", ifelse(pval<0.05, ".",""))))+
+  geom_tile()+
+  geom_text(color = "white", nudge_x = 0)+
+  redblack.col.gradient +
+  labs(y = "", x = "", 
+       caption = paste0("    ** FDR < 0.05", "\n",
+                        "    .   pval < 0.05")) +
+  my.guides
+ggsave("figs/corr-language-metrics-w-each-other.png", bg = "white",
+       width = 9, height = 10, units = "in", dpi = 360)
+# do lmer
+lmer.results <- foreach(i = 18:(ncol(m124)-2), .combine = rbind) %dopar% {
   y.var <- colnames(m124)[i]
   y <- scale(m124[,i] %>% as.matrix() %>% as.numeric(),scale = T, center = T)[,1]
   # do a one to one model
   lmer.metric.one <- foreach(j = 3:16, .combine = rbind) %dopar% {
     x.var <- colnames(m124)[j]
     x <- scale(m124[,j] %>% as.matrix() %>% as.numeric(), scale = T, center = T)[,1]
-    lm <- lmerTest::lmer(yy ~ xx + (1|prompt), 
-                         data = m124 %>% select(te_id, prompt) %>% ungroup() %>%
+    lm <- lmerTest::lmer(yy ~ xx + (1|prompt) + age + sex + age:sex, 
+                         data = m124 %>% select(te_id, prompt, age, sex) %>% ungroup() %>%
                            mutate(yy = as.numeric(y), xx = as.numeric(x)))
     res <- jtools::summ(lm, confin = T, pval = T)$coeftable %>%
       as.data.frame() %>%
@@ -221,16 +242,17 @@ lmer.results <- foreach(i = 18:ncol(m124), .combine = rbind) %dopar% {
   # didn't work
   # # make a model that uses all metrics together to predict IQ
   d2 <- m124 %>%
-    select(colnames(all), colnames(combined.metrics)) %>%
+    select(age, sex, colnames(all), colnames(combined.metrics)) %>%
     mutate(yy = y) %>%
     drop_na(yy)
   lm2 <- lmerTest::lmer(yy ~ onset + tot_comments + off_target + rep_prompt +
                           rep_words_per_prompt + rep_word_at_all + count_all +
                           full_euc_dist_normalized + global_divergence + cos_similarity +
-                          vol_value + avg_lifetime + avg_switches + communities_count +
+                          vol_value + avg_lifetime + avg_switches + communities_count + 
+                          age + sex + age:sex +
                           (1|prompt),
                         data = d2 %>%
-                          mutate_at(c(3:ncol(d2)), .funs = function(x) scale(x, scale = T, center = T)[,1]))
+                          mutate_at(c(5:ncol(d2)), .funs = function(x) scale(x, scale = T, center = T)[,1]))
   res.2 <- jtools::summ(lm2, confin = T, pval = T)$coeftable %>%
     as.data.frame() %>%
     rownames_to_column("fixed") %>%
@@ -248,8 +270,11 @@ lmer.results <- foreach(i = 18:ncol(m124), .combine = rbind) %dopar% {
 }
 # plot
 lmer.results %>%
-  # filter(fixed=="xx") %>%
-  filter(fixed!="xx") %>%
+  filter(model_type == "one_for_one",
+         fixed == "xx",
+         ! x %in% c("age", "sexM", "age:sexM")) %>%
+  # filter(model_type == "all_for_one", 
+         # ! x %in% c("age", "sexM", "age:sexM")) %>%
   mutate(sig = ifelse(pval<0.05, "pval < 0.05", "pval \u2265 0.05")) %>%
   mutate(var = sub("_age_corrected_standard_score", "_NIH", y),
          cat2 = ifelse(grepl("NIH", var), "NIH-TB", "IQ"),
@@ -267,12 +292,15 @@ lmer.results %>%
         strip.text.y.right = element_text(angle = 0)) +
   labs(x = "Estimate", y="",
        caption = paste0("n(samples): ", length(unique(m124$te_id)), "\n"))
-# ggsave("figs/lmer-iq-nih-all-metrics-random-prompt-one-for-one.png", bg = "white",
-ggsave("figs/lmer-iq-nih-all-metrics-random-prompt-all-for-one.png", bg = "white",
+ggsave("figs/lmer-iq-nih-all-metrics-random-prompt-one-for-one.png", bg = "white",
+# ggsave("figs/lmer-iq-nih-all-metrics-random-prompt-all-for-one.png", bg = "white",
        width = 18, height = 12, units = "in", dpi = 360)
 lmer.results %>%
-  # filter(fixed=="xx") %>%
-  filter(fixed!="xx") %>%
+  filter(model_type == "one_for_one",
+         fixed == "xx",
+         ! x %in% c("age", "sexM", "age:sexM")) %>%
+  # filter(model_type == "all_for_one", 
+         # ! x %in% c("age", "sexM", "age:sexM")) %>%
   mutate(sig = ifelse(pval<0.05, "pval < 0.05", "pval \u2265 0.05"),
          FDR = p.adjust(pval, method = "fdr"),
          var = sub("_age_corrected_standard_score", "_NIH", y),
@@ -290,6 +318,98 @@ lmer.results %>%
                         "    ** FDR < 0.05", "\n",
                         "    .   pval < 0.05")) +
   my.guides
+################################################################################
+################################################################################
+################################################################################
+# trying to make a clean fancy plot for these results
+library(fmsb)
+data <- rbind(rep(0.6, length(unique(lmer.results$x))-3),
+              rep(-0.3, length(unique(lmer.results$x))-3),
+              lmer.results %>% 
+                filter(model_type=="one_for_one",
+                       fixed == "xx") %>%
+                select(y, x, value = Estimate) %>%
+                filter(grepl("VCI_comp", y)|
+                         grepl("PSI_comp", y)|
+                         grepl("WM_comp", y)) %>% 
+                distinct() %>%
+                pivot_wider(names_from = "x", values_from = "value") %>%
+                column_to_rownames("y"))
+# Color vector
+colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+# plot with default options:
+lab <- c(-0.3, -.2, -.1, 0, 0.1,0.2,0.3,0.4,0.5)
+radarchart(data, axistype=1, seg = length(lab)-1, 
+           pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+           cglcol="grey", cglty=1, axislabcol="grey", caxislabels=lab, cglwd=0.8,
+           vlcex=0.7)
+legend(x=1.2, y=1, legend = sub("_composite_score", "", rownames(data[-c(1,2),])), 
+       bty = "n", pch=20 , col=colors_in , 
+       text.col = "grey", cex=0.7, pt.cex=2)
+################################################################################
+################################################################################
+# make a heatmap of estimates
+lmer.results %>% 
+  filter(model_type=="one_for_one") %>%
+  select(y, x, Estimate, pval) %>%
+  mutate(sig = ifelse(pval<0.05, "pval < 0.05", "pval \u2265 0.05"),
+         FDR = p.adjust(pval, method = "fdr"),
+         var = sub("_age_corrected_standard_score", "_NIH", y),
+         cat2 = ifelse(grepl("NIH", var), "NIH-TB", "IQ"),
+         var= sub("_NIH", "", var),
+         var = factor(var, levels = unique(var))) %>%
+  ggplot(aes(x= var, y=x, fill = Estimate, 
+             label = ifelse(FDR<0.05, "**", ifelse(pval<0.05, ".","")))) +
+  geom_tile()+
+  geom_text()+
+  redblack.col.gradient +
+  ggh4x::facet_grid2(cols = vars(cat2), scales = "free") +
+  labs(y = "", x = "", 
+       caption = paste0("n(samples): ", length(unique(m124$te_id)),"\n",
+                        "prompt was a dded as a random variable", "\n",
+                        "    ** FDR < 0.05", "\n",
+                        "    .   pval < 0.05")) +
+  my.guides
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+m124 <- read_rds("data/derivatives/all-summarized-metrics.rds")
+# try building a regression tree to predict the IQ/NIH-TB 
+library(rpart)
+i.tree <- rpart(VCI_composite_score ~ ., data = m124 %>%
+                  select(colnames(m124)[3:16], VCI_composite_score))
+summary(i.tree)
+i.tree$cptable
+# try bagging instead
+library(caret)
+all.trees <- list()
+all.trees <- foreach(i = 18:44, .combine = rbind) %dopar% {
+  y.var <- colnames(m124)[i]
+  y <- m124[,i] %>% as.matrix() %>% as.numeric()
+  # CV bagged model
+  i.tree <- rpart(
+    y ~ ., 
+    data = m124 %>%
+      select(colnames(m124)[3:16]) %>%
+      mutate(y = y) %>%
+      mutate_all(.funs = function(x) scale(x, scale = T, center = T)[,1]))
+  # get the most important features used for prediction
+  df <- data.frame(y = y.var,
+                   i.tree$variable.importance %>%
+                     as.data.frame() %>%
+                     rename(var_importance = 1) %>%
+                     rownames_to_column("var"))
+  return(df)
+}
+all.trees %>%
+  ggplot(aes(x=var_importance, y = reorder(var, desc(var_importance)))) +
+  geom_point()+
+  facet_wrap(~y, scales = "free")
 ################################################################################
 ################################################################################
 ################################################################################
